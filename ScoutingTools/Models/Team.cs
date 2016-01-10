c50920c8ae525a7ea745783aed410142475ed35a
supermeap123 <garrentvanmeter@gmail.com>
2016-01-09 16:03:34 -0800

    Added Team model

---
+++ ScoutingTools/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoutingTools.Models
+{
+    public class Team
+    {
+        public string Key { get; set; }
+        
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+
+        public ICollection<string> EventsAttended { get; set; }
+    }
+}
